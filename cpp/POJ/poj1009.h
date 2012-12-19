79c98b5b847f9025ef0ea409cf433e2547833f98
xdutaotao <xdutaotao@gmail.com>
2012-12-19 17:27:46 +0800

    modified:   cpp/POJ/poj1009.h

--- cpp/POJ/poj1009.h
+++ cpp/POJ/poj1009.h
@@ -13,52 +13,11 @@
 #include <cstring>
 using namespace std;
 
-#define CPos(x,y,w,h) (((x)<0 || (y)<0 || (x)>=(w) || (y)>=(h))? -1 : (y)*(w)+(x))
-
-#define DIFF(v1,v2) ((v1)>=(v2)? (v1)-(v2) : (v2)-(v1))
-
-#define Xpos(p,w) ((p)%(w))
-#define Ypos(p,w) ((p)/(w))
 
 const int MAXN=1010;
 unsigned char arr_v[MAXN]={0};
 unsigned int arr_n[MAXN]={0};
 
-inline int search_idx(unsigned int arr[],int beg, int end, const int& pos)
-{
-	int mid=(beg+end)>>1;
-	if(mid==0 || (pos <=arr[mid] && pos > arr[mid-1]))
-        return mid<0?0:mid;
-
-	if(arr[mid]<pos)
-		beg = mid+1;
-	else
-        end=mid;
-
-	return search_idx(arr,beg,end,pos);
-}
-
-void solve()
-{
-	unsigned int arr[]={3,7,9,14,16,24,52};
-
-	int k;
-	while(cin>>k)
-		cout<<search_idx(arr,0,sizeof(arr)/sizeof(int),k)<<endl;
-}
-
-//2-matrix to 1-array
-inline int M2A(const int& x, const int& y, const int&w, const int&h, const int&count)
-{
-	if(x <0 || y< 0 || x>= w || y >= h)
-		return -1;
-
-	int pos=y*w+x;
-
-	//二分搜索
-	int idx;
-
-}
 
 inline unsigned char get_val(const int& p, const int& cp, bool forward)
 {
@@ -80,11 +39,22 @@ inline unsigned char get_val(const int& p, const int& cp, bool forward)
 	}
 
 	return arr_v[ans_idx];
-
-
 }
 
-void solve1()
+inline unsigned char get_todo_val(int todo_pos, int& arr_todo_idx, int arr_pos_idx, bool forwarded)
+{
+    arr_todo_idx = arr_pos_idx;
+    if(forwarded)
+    {
+        while(todo_pos > arr_n[arr_todo_idx]) ++arr_todo_idx; //向前找到对应的第一个小于todo_pos的idx
+    }else{
+        while(arr_todo_idx>0 && todo_pos < arr_n[arr_todo_idx-1]) --arr_todo_idx; //回溯,找到第一个大于todo_pos的idx
+    }
+
+    return arr_v[arr_todo_idx];
+}
+
+void solve()
 {
 	int width=0,n,height=0;
 	while(1)
@@ -109,33 +79,123 @@ void solve1()
 
 		cout<<width<<" *** "<<height<<endl;
 
-		int idx=0,cur_id=0,prev_diff=-1,cumulated=0;
-		while(arr_n[cur_id]>0)
-		{
-			unsigned char cur_val=arr_v[cur_id];
-			unsigned char tmp_v,tmp_diff;
-			while(idx <= arr_n[cur_id])
-			{
-				unsigned char maxd= 0;
-
-				//int pos=CPos(x-1,y-1,width,height);
+		int max_len=width*height;
 
 
+		int old_pos_idx=0,cur_pos_idx=0;
+
+		for(int pos=0;pos<max_len;++pos)
+        {
+            //判断每一格 坐标
+            int x=pos/width,y=pos%width;
+
+            int cur_pos_idx;
+            unsigned char cur_val = get_todo_val(pos,cur_pos_idx,old_pos_idx,true); //获得当前的val和idx
+            old_pos_idx = cur_pos_idx; //记录用于下一次计算新的cur_todo_idx
+
+            int todo_pos=-1,todo_idx;
+            unsigned char todo_val(0),todo_diff(0),max_diff(0);
+
+            //11点位置
+            if(x>0 && y>0)
+            {
+                todo_pos=(x-1)*width+y-1;
+                todo_val = get_todo_val(todo_pos,todo_idx,cur_pos_idx,true); //0点位置向前
+
+                todo_diff=(todo_val>cur_val?todo_val-cur_val:cur_val-todo_val);
+
+                if(todo_diff > max_diff)
+                    max_diff=todo_diff;
+            }
+
+            //0点位置
+            if(x > 0)
+            {
+                todo_pos=(x-1)*width+y;
+                todo_val = get_todo_val(todo_pos,todo_idx,todo_idx,true);
+
+                todo_diff=(todo_val>cur_val?todo_val-cur_val:cur_val-todo_val);
+
+                if(todo_diff > max_diff)
+                    max_diff=todo_diff;
+            }
+
+            //2点位置
+            if(x>0 && y<width-1)
+            {
+                todo_pos=(x-1)*width+y+1;
+                todo_val = get_todo_val(todo_pos,todo_idx,todo_idx,true); //0点位置向前
+
+                todo_diff=(todo_val>cur_val?todo_val-cur_val:cur_val-todo_val);
+
+                if(todo_diff > max_diff)
+                    max_diff=todo_diff;
+            }
+
+            //9点位置
+            if(y > 0)
+            {
+                todo_pos=(x)*width+y-1;
+                todo_val = get_todo_val(todo_pos,todo_idx,cur_pos_idx,false);
+
+                todo_diff=(todo_val>cur_val?todo_val-cur_val:cur_val-todo_val);
+
+                if(todo_diff > max_diff)
+                    max_diff=todo_diff;
+            }
+
+            //3点位置
+            if(y<width-1)
+            {
+                todo_pos=(x)*width+y+1;
+                todo_val = get_todo_val(todo_pos,todo_idx,cur_pos_idx,true);
+
+                todo_diff=(todo_val>cur_val?todo_val-cur_val:cur_val-todo_val);
+
+                if(todo_diff > max_diff)
+                    max_diff=todo_diff;
+            }
+
+            //7点位置
+            if(x<height-1 && y>0)
+            {
+                todo_pos=(x+1)*width+y-1;
+                todo_val = get_todo_val(todo_pos,todo_idx,cur_pos_idx,true);
+
+                todo_diff=(todo_val>cur_val?todo_val-cur_val:cur_val-todo_val);
+
+                if(todo_diff > max_diff)
+                    max_diff=todo_diff;
+            }
+
+            //6点位置
+            if(x<height-1)
+            {
+                todo_pos=(x+1)*width+y;
+                todo_val = get_todo_val(todo_pos,todo_idx,todo_idx,true);
+
+                todo_diff=(todo_val>cur_val?todo_val-cur_val:cur_val-todo_val);
+
+                if(todo_diff > max_diff)
+                    max_diff=todo_diff;
+            }
+
+            //4点位置
+            if(x<height-1 && y<width-1)
+            {
+                todo_pos=(x+1)*width+y+1;
+                todo_val = get_todo_val(todo_pos,todo_idx,todo_idx,true);
+
+                todo_diff=(todo_val>cur_val?todo_val-cur_val:cur_val-todo_val);
+
+                if(todo_diff > max_diff)
+                    max_diff=todo_diff;
+            }
 
 
-				if(prev_diff == maxd)
-				{
-					++cumulated; //
-				}else{
-					cout<<prev_diff<<" "<<cumulated<<endl;
-					prev_diff = maxd;
-					cumulated = 1;
-				}
-				++idx; //goto next
-			}
-			++cur_id; //goto next element
-		}
-		cout<<"0 0"<<endl;
+            printf("%d,%d %d\n",x,y,max_diff);
+        }
+
 	}
 
 
