1a5551e407c12ba887bfea172333ca5cc99a0525
xdutaotao <xudtaotao@gmail.com>
2013-01-07 02:19:02 +0800

    modified:   cpp/leetcode.com/insert_interval.h 	modified:   cpp/leetcode.com/integer_to_roman.h 	modified:   cpp/leetcode.com/interleaving_sring.h

--- cpp/leetcode.com/interleaving_sring.h
+++ cpp/leetcode.com/interleaving_sring.h
@@ -18,6 +18,12 @@ When s3 = "aadbbcbcac", return true.
 When s3 = "aadbbbaccc", return false.
 */
 
+//判断是否可以被交叉构成,可递归搜索判断
+
+/*
+小数据通过,大数据超时了TLE
+*/
+
 #include <cstdio>
 #include <iostream>
 #include <fstream>
@@ -38,12 +44,42 @@ public:
 		// Start typing your C/C++ solution below
 		// DO NOT write int main() function    
 
+		int len_s1=s1.length(),len_s2=s2.length(),len_s3=s3.length();
+
+		if(len_s1+len_s2 != len_s3)
+			return false;
+
+		if(len_s1 == 0)
+			return s2==s3;
+
+		if(len_s2 == 0)
+			return s1==s3;
+
+		string s1_tmp=s1,s2_tmp=s2,s3_tmp=s3;
+		//以下均为非0
+		if(s1[0] == s3[0])
+		{
+			if(isInterleave(s1_tmp.erase(0,1),s2_tmp,s3_tmp.erase(0,1)))
+				return true;
+		}
+
+		s1_tmp=s1,s2_tmp=s2,s3_tmp=s3;
+
+		if(s2[0] == s3[0])
+		{
+			if(isInterleave(s1_tmp,s2_tmp.erase(0,1),s3_tmp.erase(0,1)))
+				return true;
+		}
+
+		return false;
 	}
 };
 
 void solve()
 {
-
+	Solution s;
+	cout<<s.isInterleave(string("aabcc"),string("dbbca"),string("aadbbcbcac"))<<endl;
+	cout<<s.isInterleave(string("aabcc"),string("dbbca"),string("aadbbbaccc"))<<endl;
 }
 
 #endif
