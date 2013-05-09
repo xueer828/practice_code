ae255257f389a81ad3d1ff410067d9e88d9830d3
xdutaotao <xudtaotao@gmail.com>
2013-05-10 00:32:33 +0800

    modified:   cpp/POJ/poj1035_spell_checker.h

--- cpp/POJ/poj1035_spell_checker.h
+++ cpp/POJ/poj1035_spell_checker.h
@@ -46,16 +46,75 @@ void add_node(node* dict,string s)
 	t->t = s;
 }
 
+int correct(node* dict, string& in, int n, bool fixed, vector<string>& vs)
+{
+	assert(dict);
+	if(n >= in.size())
+	{
+		if(dict->t == "" && fixed) //如果字符已经完毕,但是还剩余一个字符可以改,则继续下去
+			return 0;
+		else if(dict->t != "") 
+		{
+			vs.push_back(dict->t);
+			if(fixed)
+				return 1;
+			else
+				return 2;
+		}else{ //not node, and fixed == false
+			for(int i=0;i<26;++i)
+			{
+				node* p=dict->mp[i];
+				if(p && p->t != "")
+				{
+					vs.push_back(p->t);
+				}
+			}
+			return 1;
+		}		
+	}
+
+	//如果本层存在对应的字符,则匹配
+	node* p;
+	if(p=dict->mp[in[n]-'a'])
+	{
+		if(correct(p,in,n+1,fixed,vs))
+			return true;
+	}else{
+		for(int i=0;i<26;++i)
+		{
+			p=dict->mp[i];
+			bool flag;
+			if(!fixed) //存在字符,且未做过任何修改,则进行修改
+			{
+				//删除
+				if(correct(dict,in,n+1,true,vs))
+					break;
+
+				if(p){
+					//增加
+					if(correct(p,in,n,true,vs))
+						break;
+					//改
+					if(correct(p,in,n+1,true,vs))
+						break;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
 void solve()
 {
 	string tmp;
-	node* dict[26];
+	node dict; //字典
 	while(cin>>tmp)
 	{
 		if(tmp == "#")
 			break;
 
-		add_node(dict[tmp[0]]-'0',tmp);		
+		add_node(&dict,tmp);		
 	}
 
 	//dict is ready.
@@ -65,7 +124,16 @@ void solve()
 		if(tmp == "#")
 			break;
 
-
+		vector<string> ts;
+		if(correct(&dict,tmp,0,false,ts))
+		{
+			cout<<tmp<<" is correct"<<endl;
+		}else{
+			cout<<tmp<<":";
+			for(int i=0;i<ts.size();++i)
+				cout<<" "<<ts[i];
+			cout<<endl;
+		}
 	}
 }
 
