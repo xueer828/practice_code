0b61c1c74769689e3658cd58392c898aec8bd5a3
xdutaotao <xdutaotao@gmail.com>
2013-05-13 17:25:38 +0800

    modified:   cpp/POJ/poj1035_spell_checker.h

--- cpp/POJ/poj1035_spell_checker.h
+++ cpp/POJ/poj1035_spell_checker.h
@@ -103,7 +103,7 @@ bool correct(node* dict, string& in, int n, bool fixed, map<int,string>& vs)
 	return false;
 }
 
-void solve()
+void solve1()
 {
 	string tmp;
 	node dict; //字典
@@ -134,6 +134,152 @@ void solve()
 				cout<<" "<<i->second;
 			cout<<endl;
 		}
+	}
+}
+
+
+//方法2,不一样的存储结构
+
+struct word
+{
+	string str;
+	int seq;
+	word(string s,int l=0):seq(l),str(s){};
+};
+
+bool sort_comp(word* w1, word* w2)
+{
+	return w1->seq < w2->seq;
+}
+
+void solve()
+{
+	vector<vector<word*> > bucket(21);
+	string in;
+	int cnt=0;
+	while(cin>>in)
+	{
+		if(in == "#")
+			break;
+
+		int n=in.length();
+		assert(n>0 && n<=20);
+
+		word* w=new word(in,++cnt);
+		bucket[n].push_back(w);
+	}
+
+	vector<word*> res;
+	while(cin>>in)
+	{
+		if(in == "#")
+			break;
+
+		int len=in.length();
+		bool total_equal=false;
+		
+		//仅仅对长度为len的字典进行全比较和修改比较
+		for(int i=0;i<bucket[len].size();++i)
+		{
+			if(bucket[len][i]->str == in)
+			{
+				cout<<in<<" is correct"<<endl;
+				total_equal = true;
+				break;
+			}
+		}
+
+		if(total_equal) continue;
+
+		res.clear();
+		int i,j,k;
+		//对同等长度进行换字符比较
+		bool is_ok(false),is_changed(false);
+		for(i=0;i<bucket[len].size();++i)
+		{
+			string& tmp=bucket[len][i]->str;
+			is_ok=true,is_changed=false;
+			
+			//k为输入字符串的字符位置
+			for(j=0,k=0;j<tmp.length();++j,++k)
+			{
+				if(tmp[j] != in[k])
+					if(is_changed)
+					{
+						is_ok = false;
+						break;
+					}else
+					{
+						is_changed = true;
+					}
+			}
+
+			if(is_ok)
+				res.push_back(bucket[len][i]);
+		}
+
+		//对 长度小一进行增加不同字符判断, 字典长度 < 串长度
+		for(i=0;i<bucket[len-1].size();++i)
+		{
+			string& tmp=bucket[len-1][i]->str;
+			is_ok=true,is_changed=false;
+
+			for(j=0,k=0;k<in.length();++j,++k)
+			{
+				if(j >= tmp.length() && !is_changed) //ok can be added
+					break;
+
+				if(tmp[j] != in[k])
+					if(is_changed)
+					{
+						is_ok = false;
+						break;
+					}else{
+						is_changed=true;
+						--j; //自减一次
+					}
+			}
+
+			if(is_ok)
+				res.push_back(bucket[len-1][i]);
+		}
+
+		//对 长度大一的字符进行判断,字典长度 > 串长度
+		for(i=0;i<bucket[len+1].size();++i)
+		{
+			string& tmp=bucket[len+1][i]->str;
+			is_ok=true,is_changed=false;
+			for(j=0,k=0;j<tmp.length();++j,++k)
+			{
+				if(k >= in.length() && !is_changed) //ok
+					break;
+
+				if(tmp[j] != in[k]){
+					if(is_changed)
+					{
+						is_ok = false;
+						break;
+					}else
+					{
+						is_changed = true;
+						--k;
+					}
+				}
+			}
+
+			if(is_ok)
+				res.push_back(bucket[len+1][i]);
+		}
+
+		//对res进行排序
+		sort(res.begin(),res.end(),sort_comp);
+		
+		cout<<in<<":";
+		for(i=0;i<res.size();++i)
+		{
+			cout<<" "<<res[i]->str;
+		}
+		cout<<endl;
 	}
 }
 
---
+++ cpp/POJ/poj1068_parencodings.h
@@ -0,0 +1,38 @@
+#ifndef _TEST_
+#define _TEST_
+
+// poj1068_parencodings
+// Copyright (c) 2013/5/13 
+// Author: xdutaotao (xdutaotao@gmail.com)
+// 
+
+/*
+http://poj.org/problem?id=1068
+ */
+
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cstdlib>
+using namespace std;
+
+
+void solve()
+{
+	int n;
+	cin>>n;
+	while(n--)
+	{
+
+	}
+}
+
+#endif 
